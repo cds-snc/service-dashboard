35a0b409175e67e017ded28bdfcd097563d6bc1b
Dave Samojlenko <dsamojlenko@gmail.com>
2017-05-26 16:10:27 -0400

    created Excel import class

---
+++ app/Importers/InventoryDataImport.php
@@ -0,0 +1,12 @@
+<?php namespace App\Importers;
+
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Files\ExcelFile;
+
+class InventoryDataImport extends ExcelFile
+{
+    public function getFile()
+    {
+        return storage_path('app/service_inventory_normalized.xlsx');
+    }
+}
