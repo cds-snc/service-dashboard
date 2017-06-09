cfef518289b80adde1b66704cf852709e75d8b1b
Dave Samojlenko <dsamojlenko@gmail.com>
2017-06-09 11:18:37 -0400

    add axios polyfill for ie11 support

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
@@ -19,6 +17,7 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+require('es6-promise').polyfill();
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
